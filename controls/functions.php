26ccaed8a37d8533b60f7ca05fa883a15a1ca9fd
Isael Sousa <faelp22@hotmail.com>
2014-05-28 15:34:24 -0300

    finalizado sistema de cadastro e validação

--- controls/functions.php
+++ controls/functions.php
@@ -13,6 +13,15 @@
 				header("location: $index");
 				
 			elseif(isset($_GET['op']) && $_GET['op'] == "list"):
+				$contatos = file("database/contatos.txt");
+				
+				//titulos da tabela
+				$titulos = array("Nome", "Email", "Telefone", "Endereço", "Sexo");
+					
+				foreach($contatos as $cada_linha){
+					$dados[] = explode("#", $cada_linha);
+				}
+					
 				include_once '/var/www/agenda/views/list.html';
 				
 			elseif(isset($_GET["op"]) && $_GET['op'] == "cad"):
